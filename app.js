47ab7e25283c52800bf5a57db25eec4c0900feef
bugsy <bugsy888@gmail.com>
2016-02-05 14:16:05 +0100

    LIBR-INIT: Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+
+var app = express();
+
+var port = process.env.PORT || 5000;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+app.get('/', function (req, res) {
+    res.send('Hello there!');
+});
+
+app.get('/books', function (req, res) {
+    res.send('Hello Books');
+});
+
+app.listen(5000, function (err) {
+    console.log('Running on port: ' + port);
+});
