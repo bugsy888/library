4b4984beefb7bf85842a107cf0669a2ca055ab0d
bugsy <bugsy888@gmail.com>
2016-02-26 12:03:21 +0100

    using nodeJS oracledb

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,31 @@ gulp.task('style', function () {
         .pipe(jscs());
 });
 
-gulp.task('serve', ['style'], function () {
+gulp.task('inject', function(){
+    var wiredep = require('wiredep').stream;
+    var inject = require('gulp-inject');
+
+    var injectSrc = gulp.src(['./public/css/*.css',
+                              './public/js/*.js'], {read: false});
+
+    var injectOptions = {
+        ignorePath: '/public'
+    };
+
+    var options = {
+        bowerJson: require('./bower.json'),
+        directory: './public/lib',
+        ignorePath: '../../public'
+    };
+
+    return gulp.src('./src/views/*.html')
+        .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOptions))
+        .pipe(gulp.dest('./src/views'));
+
+});
+
+gulp.task('serve', ['style', 'inject'], function () {
     var options = {
         script: 'app.js',
         delayTime: 1,
---
+++ src/routes/bookRoutes.js
@@ -0,0 +1,84 @@
+var express = require('express');
+var oracledb = require('oracledb');
+var bookRouter = express.Router();
+
+oracledb.outFormat = oracledb.OBJECT;
+
+var router = function(nav) {
+    bookRouter.route('/')
+        .all(function (req, res, next) {
+            oracledb.getConnection(
+                {
+                    user: 'plural',
+                    password: 'S1ght',
+                    connectString: 'nodetest'
+                },
+                function (err, connection) {
+                    if (err) {
+                        console.error(err.message);
+                        return;
+                    }
+                    connection.execute(
+                        'SELECT * FROM books ',
+                        function (err, result) {
+                            if (err) {
+                                console.error(err.message);
+                                return;
+                            } else {
+                                req.books = result.rows;
+                                next();
+                            }
+                        }
+                    );
+                }
+            );
+        })
+        .get(function (req, res) {
+            res.render('bookListView', {
+                title: 'Books',
+                nav: nav,
+                books: req.books
+            });
+        });
+
+    bookRouter.route('/:id')
+        .all(function(req, res, next) {
+            oracledb.getConnection(
+                {
+                    user: 'plural',
+                    password: 'S1ght',
+                    connectString: 'nodetest'
+                },
+                function (err, connection) {
+                    if (err) {
+                        console.error(err.message);
+                        return;
+                    }
+                    connection.execute(
+                        'SELECT * FROM books WHERE ID = :id',
+                        [req.params.id],
+                        function (err, result) {
+                            if (err) {
+                                console.error(err.message);
+                                return;
+                            } else {
+                                req.book = result.rows[0];
+                                next();
+                            }
+                        }
+                    );
+                }
+            );
+        })
+        .get(function(req, res) {
+            res.render('bookView', {
+                title: 'Book',
+                nav: nav,
+                book: req.book
+            });
+        });
+
+    return bookRouter;
+};
+
+module.exports = router;
